c62bde318b00bbc4d6c287919733c8aa88a4b66e
TheMave <marius.versteegen@gmail.com>
2024-05-16 11:07:15 +0200

    moved alternative view to github pages

--- programma/DrawioProgrammaNaarMd.py
+++ programma/DrawioProgrammaNaarMd.py
@@ -102,7 +102,7 @@ def generate_markdown(intro, sprints, projects, weeks, sessions):
     md_content = []
     md_content.append(intro)
     md_content.append("## Programma")
-    md_content.append("[(alternatieve view)](./Programma.drawio.html)")
+    md_content.append("[(alternatieve view)](https://hu-ti-dev.github.io/TI-S3/programma/Programma.drawio.html)")
     md_content.append("")
 
     while sprints:
